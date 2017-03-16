49b872d29493d69a9ec7c2b2bc626278a15d790a
mikedacoder <michaeldryburgh@hotmail.com>
2017-03-16 00:08:10 -0600

    Added functionality to previous and next buttons

--- scripts/main.js
+++ scripts/main.js
@@ -15,6 +15,7 @@ function initAudioPlayer() {
 			"Tenor1_There_Will_Be_Rest", 
 			"Tenor2_There_Will_Be_Rest"
 			];
+	var seeking;
 	
 	//Set object references
 	var playbtn = document.getElementById("playpausebtn");
@@ -28,13 +29,21 @@ function initAudioPlayer() {
 	
 	//Audio Object
 	var audio = new Audio();
-	audio.src = dir + playlist[0] + ext;
-	audio.play();
+	audio.src = dir + playlist[playlist_index] + ext;
+	audio.pause();
 	
 	//Add event handlers
 	playbtn.addEventListener("click", playPause);
 	volumeslider.addEventListener("mousemove", setVolume);
+	mutebtn.addEventListener("click", mute);
+	seekslider.addEventListener("mousedown", function(event){ seeking = true; seek(event); });
+	seekslider.addEventListener("mousemove", function(event){ seek(event); });
+	seekslider.addEventListener("mouseup", function(){ seeking = false; });
+	audio.addEventListener("timeupdate", function(){ seekTimeUpdate(); });
+	prevbtn.addEventListener("click", prevTrack);
+	nextbtn.addEventListener("click", nextTrack);
 	
+	//Play and pause the audio
 	function playPause() {
 		if(audio.paused) {
 			audio.play();
@@ -47,8 +56,81 @@ function initAudioPlayer() {
 		}
 	}
 	
+	//Change the volume level
 	function setVolume() {
 		audio.volume = volumeslider.value / 100;
+	}
+	
+	// Mute and unmute the audio
+	function mute() {
+		if(audio.muted) {
+			audio.muted = false;
+			mutebtn.style.background = "url(images/MuteButton.png) no-repeat";
+			mutebtn.style.backgroundSize = "100% 100%";
+		} else {
+			audio.muted = true;
+			mutebtn.style.background = "url(images/UnMuteButton.png) no-repeat";
+			mutebtn.style.backgroundSize = "100% 100%";
+		}
+	}
+	
+	//Seek - Not working correctly
+	function seek(event) {
+		if(seeking){
+			seekslider.value = event.clientX - seekslider.offsetLeft;
+			var seekto = audio.duration * (seekslider.value / 100);
+			audio.currentTime = seekto;
+		}
+	}
+	
+	// Keep the displayed time current
+	function seekTimeUpdate() {
+		var nt = audio.currentTime * (100 / audio.duration);
+		seekslider.value = nt;
+		var curmins = Math.floor(audio.currentTime / 60);
+		var cursecs = Math.floor(audio.currentTime - curmins * 60);
+		var durmins = Math.floor(audio.duration / 60);
+		var dursecs = Math.floor(audio.duration - durmins * 60);
+		if(cursecs < 10) { cursecs = "0" + cursecs; }
+		if(dursecs < 10) { dursecs = "0" + dursecs; }
+		if(curmins < 10) { curmins = "0" + curmins; }
+		if(durmins < 10) { durmins = "0" + durmins; }
+		currtime.innerHTML = curmins + ":" + cursecs;
+		durationtime.innerHTML = durmins + ":" + dursecs;
+	}
+	
+	//Switch to the previous track
+	function prevTrack() {
+		if(playlist_index === 0) {
+			playlist_index = (playlist.length - 1);
+		} else {
+			playlist_index--;
+		}
+		audio.src = dir + playlist[playlist_index] + ext;
+		audio.currentTime = 0;
+		console.log(playlist[playlist_index]);
+		if(audio.paused) {
+			audio.pause();
+		} else {
+			audio.play();
+		}		
+	}
+	
+	//Switch to the next track
+	function nextTrack() {
+		if(playlist_index === (playlist.length - 1)) {
+			playlist_index = 0;
+		} else {
+			playlist_index++;
+		}
+		audio.src = dir + playlist[playlist_index] + ext;
+		audio.currentTime = 0;
+		console.log(playlist[playlist_index]);
+		if(audio.paused) {
+			audio.pause();
+		} else {
+			audio.play();
+		}		
 	}
 	
 }
